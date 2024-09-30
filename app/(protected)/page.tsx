6c7cf47030ef1c4956fdabba350c3c88d9654540
shahriarAS <shovonshahriar01@gmail.com>
2024-10-01 00:00:35 +0600

    fix: bad client count

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -16,8 +16,6 @@ export type PositionType = {
   imageSrc: StaticImageData;
 };
 
-let clientCounts = { client1: 0, client2: 0, client3: 0 };
-
 const Home = () => {
   const {
     gameStarted,
@@ -33,111 +31,67 @@ const Home = () => {
     settings?.levels?.[currentLevel]?.clientDuration || 1300;
 
   const [clicked, setClicked] = useState(-1);
+  const [allClients, setAllClients] = useState<PositionType[]>([]);
   const [selectedClients, setSelectedClients] = useState<PositionType[]>([]);
 
-  const initializeClientCounts = (
-    totalClient: number,
-    totalClient2: number,
-    totalClient3: number
-  ) => {
-    const remainingClients = totalClient - totalClient2 - totalClient3;
+  const initializeClientCounts = () => {
+    const totalClient = settings?.levels?.[currentLevel]?.totalClient;
+    const totalClient2 = settings?.levels?.[currentLevel]?.client2Count || 0;
+    const totalClient3 = settings?.levels?.[currentLevel]?.client3Count || 0;
 
-    clientCounts.client1 = Math.max(
-      2,
-      Math.floor(Math.random() * (remainingClients + 1))
+    const clients: PositionType[] = [];
+
+    clients.push(
+      ...Array(totalClient - totalClient2 - totalClient3).fill(
+        settings?.clientProperties?.client1
+      )
+    );
+    clients.push(
+      ...Array(totalClient2).fill(settings?.clientProperties?.client2)
+    );
+    clients.push(
+      ...Array(totalClient3).fill(settings?.clientProperties?.client3)
     );
 
-    clientCounts.client2 = totalClient2;
-    clientCounts.client3 = totalClient3;
-
-    console.log("Client Counts", clientCounts);
-  };
-
-  const resetClientCounts = () => {
-    initializeClientCounts(
-      settings?.levels?.[currentLevel]?.totalClient,
-      settings?.levels?.[currentLevel]?.client2Count || 0,
-      settings?.levels?.[currentLevel]?.client3Count || 0
-    );
+    setAllClients(clients);
+    // allClients = clients;
   };
 
   const selectRandomClients = () => {
-    const { client1, client2, client3 } = settings?.clientProperties;
-    const totalRemaining =
-      clientCounts.client1 + clientCounts.client2 + clientCounts.client3;
+    const clients: PositionType[] = [];
+    const clientsToShow = Number(currentLevel.split("level")[1]) >= 3 ? 4 : 1;
 
-    if (totalRemaining === 0) {
-      resetClientCounts();
-    }
+    for (let i = 0; i < clientsToShow; i++) {
+      const randomIndex = Math.floor(Math.random() * allClients.length);
 
-    let clients: PositionType[] = [];
+      const selectedClient = allClients[randomIndex];
 
-    // Show multiple clients only for levels 3 and 4
-    if (Number(currentLevel.split("level")[1]) >= 3) {
-      const clientsToShow = 4;
-
-      for (let i = 0; i < clientsToShow; i++) {
-        const rand = Math.random();
-        let selectedClient: PositionType;
-
-        if (rand < clientCounts.client1 / totalRemaining) {
-          selectedClient = client1;
-          clientCounts.client1--;
-        } else if (
-          rand <
-          (clientCounts.client1 + clientCounts.client2) / totalRemaining
-        ) {
-          selectedClient = client2;
-          clientCounts.client2--;
-        } else {
-          selectedClient = client3;
-          clientCounts.client3--;
-        }
-
-        clients.push(selectedClient);
-      }
-    } else {
-      // For levels below 3, only select a single client
-      const rand = Math.random();
-      let selectedClient: PositionType;
-
-      if (rand < clientCounts.client1 / totalRemaining) {
-        selectedClient = client1;
-        clientCounts.client1--;
-      } else if (
-        rand <
-        (clientCounts.client1 + clientCounts.client2) / totalRemaining
-      ) {
-        selectedClient = client2;
-        clientCounts.client2--;
-      } else {
-        selectedClient = client3;
-        clientCounts.client3--;
-      }
+      setAllClients((prevClients) =>
+        prevClients.filter((_, index) => index != randomIndex)
+      );
 
       clients.push(selectedClient);
     }
 
     return clients;
   };
+
   const handleTap = (selectedClient: PositionType) => {
-    if (selectedClient.clientType === "good") {
-      setTotalScore(selectedClient.orders);
-      setTapCount({ byCorrect: 1, byWrong: 0, byMissed: 0 });
-    } else {
-      setTotalScore(-20);
-      setTapCount({ byCorrect: 0, byWrong: 1, byMissed: 0 });
-    }
+    const scoreAdjustment =
+      selectedClient.clientType === "good" ? selectedClient.orders : -20;
+    const tapAdjustments =
+      selectedClient.clientType === "good"
+        ? { byCorrect: 1, byWrong: 0, byMissed: 0 }
+        : { byCorrect: 0, byWrong: 1, byMissed: 0 };
+
+    setTotalScore(scoreAdjustment);
+    setTapCount(tapAdjustments);
   };
 
   const handleMissedTap = () => {
     selectedClients.forEach((client) => {
       if (client.clientType === "good") {
-        setTapCount({
-          byCorrect: 0,
-          byWrong: 0,
-          byMissed: 1,
-        });
+        setTapCount({ byCorrect: 0, byWrong: 0, byMissed: 1 });
       }
     });
   };
@@ -145,48 +99,43 @@ const Home = () => {
   const switchImage = () => {
     setClicked(-1);
 
-    let selectedDivIndexes = Array.from(
-      { length: Number(currentLevel.split("level")[1]) >= 3 ? 4 : 1 },
-      () => Math.floor(Math.random() * 9)
+    const clientsToShow = Number(currentLevel.split("level")[1]) >= 3 ? 4 : 1;
+    let selectedDivIndexes = Array.from({ length: clientsToShow }, () =>
+      Math.floor(Math.random() * 9)
     );
 
     while (
       JSON.stringify(activeDiv.sort()) ===
       JSON.stringify(selectedDivIndexes.sort())
     ) {
-      selectedDivIndexes = Array.from(
-        { length: Number(currentLevel.split("level")[1]) >= 3 ? 4 : 1 },
-        () => Math.floor(Math.random() * 9)
+      selectedDivIndexes = Array.from({ length: clientsToShow }, () =>
+        Math.floor(Math.random() * 9)
       );
     }
 
-    const newClients = selectRandomClients();
-
-    setSelectedClients(newClients);
+    if (allClients.length === 3) {
+      initializeClientCounts();
+    }
+    setSelectedClients(selectRandomClients());
     setActiveDiv(selectedDivIndexes);
   };
 
   useEffect(() => {
-    let interval: NodeJS.Timeout | undefined;
-
     if (gameStarted) {
-      interval = setInterval(switchImage, switchInterval);
+      const interval = setInterval(switchImage, switchInterval);
+      return () => clearInterval(interval);
     }
-
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
   }, [gameStarted, activeDiv, switchInterval]);
 
   useEffect(() => {
-    if (activeDiv && activeDiv.length > 0) {
-      setTimeout(() => {
-        handleMissedTap();
-      }, switchInterval - 500);
+    if (activeDiv.length > 0) {
+      setTimeout(handleMissedTap, switchInterval - 500);
     }
   }, [activeDiv]);
+
+  useEffect(() => {
+    initializeClientCounts();
+  }, [gameStarted]);
 
   return (
     <React.Fragment>
