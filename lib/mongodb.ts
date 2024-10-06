0012bda0af6491ff3cc40386824f8711bdf7cfd4
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-10-07 03:06:50 +0600

    update

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,24 +2,23 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-//let cached = (global as any).mongoose || { conn: null, promise: null };
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 const connectDB = async () => {
-    // if (cached.conn) return cached.conn;
+    if (cached.conn) return cached.conn;
 
     if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-    // cached.promise =
-    //     cached.promise ||
-
-       const conn = mongoose.connect(MONGODB_URI, {
+    cached.promise =
+        cached.promise ||
+        mongoose.connect(MONGODB_URI, {
             dbName: process.env.DB_NAME,
             bufferCommands: false,
         });
-        return conn;
-    // cached.conn = await cached.promise;
 
-    // return cached.conn;
+    cached.conn = await cached.promise;
+
+    return cached.conn;
 };
 
 export default connectDB;
